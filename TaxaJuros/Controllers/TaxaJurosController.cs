ab6e39b7584fbeef627425244bc98d224046ffd7
Junior Schroder <juniorschroder@MacBook-Air-de-Junior.local>
2020-06-28 01:26:35 -0300

    Adicionado projeto webapi TaxaJuros à solução

---
+++ TaxaJuros/Controllers/TaxaJurosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaxaJuros.Controllers
+{
+    [ApiController]
+    [Route("/taxaJuros")]
+    public class TaxaJurosController : ControllerBase
+    {
+        [HttpGet]
+        public decimal Get()
+        {
+            return 0.01m;
+        }
+    }
+}
